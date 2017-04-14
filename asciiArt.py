15b7d2ee4c192585afca718748323cf909770511
Sirajus Salekin <ssalek14@earlham.edu>
2017-04-14 07:00:49 +0000

    modified:   asciiArt.py 	modified:   fileInput.py 	modified:   install.sh 	deleted:    requirements.pip 	modified:   runtime.log

--- asciiArt.py
+++ asciiArt.py
@@ -5,15 +5,7 @@ def ascii_hist(dataset):
     """Using ascii_graph library, this function
     produces a pretty command line histogram output.
     """
-
-	graph = Pyasciigraph(
-    	line_length=80,
-    	min_graph_length=1,
-    	separator_length=2,
-    	multivalue=False,
-    	human_readable='None',
-    	graphsymbol='*'
-    	)
-
-	for line in graph.graph(label="Most Frequent", data = dataset):
+    graph = Pyasciigraph(line_length=80,min_graph_length=1,separator_length=2,multivalue=False,human_readable='None', graphsymbol='*')
+    
+    for line in graph.graph(label="Most Frequent", data = dataset):
     		print(line)
